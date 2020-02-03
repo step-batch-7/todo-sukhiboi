7ee5d9e2dd6d0e137245586ec9cf1f8c5590b37d
sukhiboi <41sukhdevsingh@gmail.com>
2020-02-03 12:31:18 +0530

    Formatted test and added a test for toJSOn of TodoApp class

--- test/testTodoApp.js
+++ test/testTodoApp.js
@@ -3,7 +3,7 @@ const { Todo } = require('./../lib/todo.js');
 const { TodoList } = require('./../lib/todoList');
 const { TodoApp } = require('./../lib/todoApp');
 
-describe('createList()', () => {
+describe('TodoApp()', () => {
   let todoApp;
   let date;
 
@@ -12,20 +12,38 @@ describe('createList()', () => {
     date = new Date();
   });
 
-  it('should create a new TodoList in the App', () => {
-    todoApp.createList(new TodoList('myList'));
-    assert.deepStrictEqual(todoApp.toJSON(), '[[]]');
+  describe('createList()', () => {
+    it('should create a new TodoList in the App', () => {
+      todoApp.createList(new TodoList('myList'));
+      assert.deepStrictEqual(todoApp.toJSON(), '[[]]');
+    });
   });
 
-  it('should find a list with its name when it have some todos', () => {
-    todoApp.createList(new TodoList('myNewList'));
-    const list = new TodoList('myNewList2');
-    list.addTodo(new Todo(23, 'something', date));
-    todoApp.createList(list);
-    const foundedTodoList = todoApp.findList('myNewList2');
-    assert.deepStrictEqual(
-      foundedTodoList.toJSON(),
-      `[{"title":"something","date":"${date.toJSON()}","isCompleted":false,"id":23}]`
-    );
+  describe('toJSON()', () => {
+    it('should return the JSON representaion of the lists', () => {
+      todoApp.createList(new TodoList('myNewList'));
+      const list = new TodoList('myNewList2');
+      list.addTodo(new Todo(23, 'something', date));
+      todoApp.createList(list);
+      const foundedTodoList = todoApp.findList('myNewList2');
+      assert.deepStrictEqual(
+        foundedTodoList.toJSON(),
+        `[{"title":"something","date":"${date.toJSON()}","isCompleted":false,"id":23}]`
+      );
+    });
+  });
+
+  describe('findList()', () => {
+    it('should find a list with its name when it have some todos', () => {
+      todoApp.createList(new TodoList('myNewList'));
+      const list = new TodoList('myNewList2');
+      list.addTodo(new Todo(23, 'something', date));
+      todoApp.createList(list);
+      const foundedTodoList = todoApp.findList('myNewList2');
+      assert.deepStrictEqual(
+        foundedTodoList.toJSON(),
+        `[{"title":"something","date":"${date.toJSON()}","isCompleted":false,"id":23}]`
+      );
+    });
   });
 });

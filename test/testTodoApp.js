ad774b04f9a1293746e524e787a425a9279ddef4
sukhiboi <41sukhdevsingh@gmail.com>
2020-02-03 14:34:03 +0530

    Implemened toHtml method of TodoApp
    Now you can call toHtml and you will get the whole app as HTML.

--- test/testTodoApp.js
+++ test/testTodoApp.js
@@ -46,4 +46,19 @@ describe('TodoApp()', () => {
       );
     });
   });
+
+  describe('toHTML()', () => {
+    it('should return the HTML representaion of the lists', () => {
+      const list = new TodoList('myNewList2');
+      list.addTodo(new Todo(23, 'something', date));
+      const list2 = new TodoList('mynewList');
+      list2.addTodo(new Todo(233, 'something big', date));
+      todoApp.createList(list2);
+      todoApp.createList(list);
+      assert.deepStrictEqual(
+        todoApp.toHTML(),
+        `<div class='todo flex ' id="233"><div class="checkBox"><div class="box" onclick="toggleTodo()"></div><div class="check center hidden" onclick="toggleTodo()"></div></div><span class='content'>something big</span><div class="bin" onclick="deleteTodo()"><img src="./images/bin.png" /></div></div> <div class='todo flex ' id="23"><div class="checkBox"><div class="box" onclick="toggleTodo()"></div><div class="check center hidden" onclick="toggleTodo()"></div></div><span class='content'>something</span><div class="bin" onclick="deleteTodo()"><img src="./images/bin.png" /></div></div>`
+      );
+    });
+  });
 });
